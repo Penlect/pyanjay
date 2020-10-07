574c8aca827a526800cd39ca223012d2085b1661
Daniel Andersson <daniel.andersson@endian.se>
2020-10-07 19:53:31 +0200

    Fix missing mbedtls

---
+++ .travis-get-mbedtls.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+if [ $AUDITWHEEL_ARCH == "x86_64" ]; then
+    yum install -y mbedtls-devel zlib-devel
+elif [ $AUDITWHEEL_ARCH == "i686" ]; then
+    yum install -y mbedtls-devel zlib-devel
+elif [ $AUDITWHEEL_ARCH == "aarch64" ]; then
+    yum install -y wget
+    wget https://download-ib01.fedoraproject.org/pub/epel/testing/7/aarch64/Packages/m/mbedtls-2.7.12-1.el7.aarch64.rpm
+    rpm -Uvh ./mbedtls-2.7.12-1.el7.aarch64.rpm
+    wget https://download-ib01.fedoraproject.org/pub/epel/testing/7/aarch64/Packages/m/mbedtls-devel-2.7.12-1.el7.aarch64.rpm
+    rpm -Uvh ./mbedtls-devel-2.7.12-1.el7.aarch64.rpm
+fi
---
+++ test/test_import.py
@@ -0,0 +1,12 @@
+
+# Built-in
+import unittest
+
+
+class TestImport(unittest.TestCase):
+
+    def test_dm(self):
+        import pyanjay.dm
+
+    def test_anjay(self):
+        import pyanjay.anjay

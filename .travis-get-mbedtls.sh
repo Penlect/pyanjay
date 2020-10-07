7af693ac98ec389c8f32c2d31146003416d33219
Daniel Andersson <daniel.andersson@endian.se>
2020-10-07 20:42:17 +0200

    Removed mac build due to _pthread_condattr_setclock

--- .travis-get-mbedtls.sh
+++ .travis-get-mbedtls.sh
@@ -4,6 +4,8 @@ if [ $AUDITWHEEL_ARCH == "x86_64" ]; then
     yum install -y mbedtls-devel zlib-devel
 elif [ $AUDITWHEEL_ARCH == "i686" ]; then
     yum install -y mbedtls-devel zlib-devel
+elif [ $AUDITWHEEL_ARCH == "s390x" ]; then
+    yum install -y mbedtls-devel zlib-devel
 elif [ $AUDITWHEEL_ARCH == "aarch64" ]; then
     yum install -y wget
     wget https://download-ib01.fedoraproject.org/pub/epel/testing/7/aarch64/Packages/m/mbedtls-2.7.12-1.el7.aarch64.rpm
